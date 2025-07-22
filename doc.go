a1f76757c86755ac4024b005c38f129c8eb7de5c
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2025-07-22 05:00:11 +0000

    Implement core structures for node operations and extensions

---
+++ doc.go
@@ -0,0 +1,6 @@
+ 
+	// structure :
+	// package (node) manages the middle client node, which interacts with the Ethereum network and other middle clients.
+	// It maintains the p2p network, API endpoints, registers extensions, reputation management, and monitors node state.
+
+	// 
--- node/node.go
+++ node/node.go
@@ -1,40 +1,39 @@
 package node
 
 import (
-	"crypto/ecdsa"
 	"math/big"
 
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
 	geth "github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
+// Node represents a middle client node that interacts with the Ethereum network and other middle clients, it's to maintain the p2p network and API endpoints.
 type Node struct {
 	eth     *geth.Node
 	chainID *big.Int
 
-	supportedConsensusContract         []common.Address
+	supportedConsensusContracts        []common.Address
 	supportedExtensionRegistryContract common.Address
-	PrivateKey                         *ecdsa.PrivateKey // For signing messages and transactions
+
+	wallet *accounts.Manager // for tx signing and account management
+
+	dirLock *flock.Flock // prevents concurrent use of instance directory
 
 	ExtensionPool map[string]*Extension // Registered Extensions (ExtensionID -> Extension)
-	dirLock       *flock.Flock          // prevents concurrent use of instance directory
 
-	OperationPool map[string]*Operation // Hash -> Operation
-	PostOpPool    map[string]*PostOp    // Hash  ->PostOp
+	Monitor *Monitor // constantly monitors the node's state and health.
 
-	proofPool  map[string]*Proof  // Hash  -> Proof
-	Reputation *ReputationManager // Tracks node + extension reputation
+	Reputation *ReputationManager // Tracks node + extension reputation + participating in peer challenge-response protocol
 	config     *Config            // Custom config (timeouts, process window ...)
 	P2P        *p2p.Server        // Handles gossip and discovery
 	rpcAPIs    []rpc.API          // List of APIs currently provided by the node
 	endpoints  *servers           // Node endpoints for rpc handling
 
-	ProofVerifier *ProofVerifier // Local verifier interface
-	TxSubmitter   *TxSubmitter   // For L1 tx submission logic
-
-	state     int            // Tracks state of node lifecycle
-	EventChan chan NodeEvent // Internal event bus (Operation received, Proof verified)
-	databases *Databases     // Database handles for operations, proofs, etc.
+	state     int         // Tracks state of node lifecycle
+	Event     *event.Feed // Internal event bus (Operation received, Proof verified)
+	databases *Databases  // Database handles for operations, proofs, etc.
 	Shutdown  chan struct{}
 }
