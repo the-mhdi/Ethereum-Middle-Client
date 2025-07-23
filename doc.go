652afda500f41144bd27a0825b50445e570fc752
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2025-07-23 01:45:27 +0000

    Add Database interface and update Node struct with new fields

--- doc.go
+++ doc.go
@@ -3,4 +3,5 @@
 	// package (node) manages the middle client node, which interacts with the Ethereum network and other middle clients.
 	// It maintains the p2p network, API endpoints, registers extensions, reputation management, and monitors node state.
 
-	// 
+	// THE simlpe FLOW :
+	// op -> middle node -> reputaionSystem -> opPool -> respective extension -> postOpPool + proof -> middle node verifier -> eth txn -> submit to L1 
--- node/node.go
+++ node/node.go
@@ -7,13 +7,14 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	geth "github.com/ethereum/go-ethereum/node"
 	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
 )
 
 // Node represents a middle client node that interacts with the Ethereum network and other middle clients, it's to maintain the p2p network and API endpoints.
 type Node struct {
 	eth     *geth.Node
 	chainID *big.Int
+	config  *Config
+	Event   *event.Feed // Internal event bus (Operation received, Proof verified)
 
 	supportedConsensusContracts        []common.Address
 	supportedExtensionRegistryContract common.Address
@@ -22,18 +23,16 @@ type Node struct {
 
 	dirLock *flock.Flock // prevents concurrent use of instance directory
 
-	ExtensionPool map[string]*Extension // Registered Extensions (ExtensionID -> Extension)
+	// ExtensionPool map[string]*Extension // Registered Extensions (ExtensionID -> Extension)
 
-	Monitor *Monitor // constantly monitors the node's state and health.
-
+	Monitor    *Monitor           // constantly monitors the node's state and health.
 	Reputation *ReputationManager // Tracks node + extension reputation + participating in peer challenge-response protocol
-	config     *Config            // Custom config (timeouts, process window ...)
-	P2P        *p2p.Server        // Handles gossip and discovery
-	rpcAPIs    []rpc.API          // List of APIs currently provided by the node
-	endpoints  *servers           // Node endpoints for rpc handling
 
-	state     int         // Tracks state of node lifecycle
-	Event     *event.Feed // Internal event bus (Operation received, Proof verified)
-	databases *Databases  // Database handles for operations, proofs, etc.
-	Shutdown  chan struct{}
+	P2P       *p2p.Server // Handles gossip and discovery
+	endpoints *servers    // Node endpoints for rpc handling
+
+	db Database
+
+	state    int // Tracks state of node lifecycle
+	Shutdown chan struct{}
 }
