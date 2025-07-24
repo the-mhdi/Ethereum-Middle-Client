82039f43da2a304ed1827c7c1c1e69ddadcf4409
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2025-07-24 14:54:26 +0000

    Add constants for reputation calculation: PRIOR_VALID_PROOFS and PRIOR_INVALID_PROOFS

---
+++ reputation/constants.go
@@ -0,0 +1,6 @@
+package reputation
+
+var (
+	PRIOR_VALID_PROOFS   = 10 // Default number of valid proofs for reputation calculation
+	PRIOR_INVALID_PROOFS = 1  // Default number of invalid proofs for reputation calculation
+)
