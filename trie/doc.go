f73904261631055c7286e47d3b257c286d74bb41
Mehdi karimi <127137582+the-mhdi@users.noreply.github.com>
2025-07-29 06:00:45 +0000

    add verkle trie implementation.

---
+++ trie/doc.go
@@ -0,0 +1,9 @@
+package trie
+
+/*
+
+ package that provides an implementation of the Verkle Trie,
+ it'll be used to store the state of middle nodes, extensions and the Ethereum blockchain in a more efficient way
+ and achieve full statelessness on middle layer!
+
+*/
